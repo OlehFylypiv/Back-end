36ea69a4d978a509180fd2153c6b5ac589f45b3b
Oleh <oleh.fylypiv@gmail.com>
2017-07-15 12:33:32 +0300

    "LearnNodeJS: Improved API, added URL validation and sorting by query parameters"

---
+++ LearnNodeJS_03/db.js
@@ -0,0 +1,102 @@
+module.exports = [
+    {
+        'name': 'Oleh',
+        'scoreTime': 10,
+        'scoreEat': 5
+    },
+    {
+        'name': 'John',
+        'scoreTime': 100,
+        'scoreEat': 55
+    },
+    {
+        'name': 'Mykola',
+        'scoreTime': 110,
+        'scoreEat': 2
+    },
+    {
+        'name': 'Shtefan',
+        'scoreTime': 257,
+        'scoreEat': 35
+    },
+    {
+        'name': 'Lili',
+        'scoreTime': 86,
+        'scoreEat': 45
+    },
+    {
+        'name': 'Keit',
+        'scoreTime': 46,
+        'scoreEat': 5
+    },
+    {
+        'name': 'Vova',
+        'scoreTime': 80,
+        'scoreEat': 67
+    }, 
+    {
+        'name': 'Roman',
+        'scoreTime': 200,
+        'scoreEat': 70
+    },
+    {
+        'name': 'Denys',
+        'scoreTime': 207,
+        'scoreEat': 20
+    },
+    {
+        'name': 'Bob',
+        'scoreTime': 46,
+        'scoreEat': 34
+    },
+    {
+        'name': 'Julia',
+        'scoreTime': 145,
+        'scoreEat': 78
+    },
+    {
+        'name': 'Arsen',
+        'scoreTime': 154,
+        'scoreEat': 80
+    },
+    {
+        'name': 'Marek',
+        'scoreTime': 8,
+        'scoreEat': 1
+    },
+    {
+        'name': 'Shmidt',
+        'scoreTime': 45,
+        'scoreEat': 13
+    }, 
+    {
+        'name': 'Anna',
+        'scoreTime': 78,
+        'scoreEat': 12
+    },
+    {
+        'name': 'Kurt',
+        'scoreTime': 199,
+        'scoreEat': 50
+    },
+    {
+        'name': 'Oksana',
+        'scoreTime': 67,
+        'scoreEat': 7
+    },
+     {
+        'name': 'Bella',
+        'scoreTime': 2,
+        'scoreEat': 1
+    },
+    {
+        'name': 'Ivan',
+        'scoreTime': 0,
+        'scoreEat': 0
+    },
+    {
+        'name': 'Bein',
+        'scoreTime': 50,
+        'scoreEat': 50
+    }
+];
