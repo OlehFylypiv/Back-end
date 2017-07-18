2da4f949bd28f5789e5a75e28ad4a8bfd371ffc7
Oleh <oleh.fylypiv@gmail.com>
2017-07-18 18:59:03 +0300

    "LearnNodeJS: Improved API, fix the all bugs of the previous version"

---
+++ LearnNodeJS_04/api.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const router = express.Router();
+
+let users = require('./db');
+const bodyParser = require('body-parser');
+
+const Joi = require('joi');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+
+router.get('/users', (req, res, next) => {
+    
+    // function getParameterByName(name) {
+        
+    //     let url = req.url;
+        
+    //     name = name.replace(/[\[\]]/g, '\\$&');
+        
+    //     let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+        
+    //     let results = regex.exec(url);
+        
+    //     if (!results) {
+    //         return null;
+    //     }
+    
+    //     if (!results[2]) {
+    //         return '';
+    //     }
+    
+    //     return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    // }
+    
+    // let parametr = getParameterByName('top');
+
+    let parametr = req.query;
+    
+    if (parametr) {
+        
+        function maxValue(users, parametr) {
+        
+            let len = users.length;
+            let buffArray = [];
+
+            for (let i = 0; i < len; i++) {
+                buffArray.push(users[i][`${ parametr }`]);
+            }
+            
+            return Math.max(...buffArray);
+        }
+    
+        let max = maxValue(users, parametr.top); 
+        
+        res.send(`${ max }`);
+    }
+    else {
+        res.send(users);        
+    }
+});
+
+
+router.get('/users/:name', (req, res, next) => {
+    
+    const user = users.find( (user) => {
+        return user.name === req.params.name; // don`t work without return
+    });
+
+    res.send(user);
+});
+
+
+router.post('/users', (req, res, next) => {
+    
+    // let user = {
+    //     name: req.body.name,
+    //     scoreTime: req.body.scoreTime, // scoreTime and scoreEat are strings
+    //     scoreEat: req.body.scoreEat
+    // };
+    
+    let user = req.body;
+   
+    let schema = Joi.object({
+        name: Joi.string().min(3).max(12).required(),
+        scoreTime: Joi.string().min(1).max(6).required(),
+        scoreEat: Joi.string().min(1).max(6).required()
+    });
+
+    let resultValidation =  Joi.validate(user, schema);
+    
+    if (resultValidation.error) {
+        throw new Error(resultValidation.error);
+    }
+
+    function  isSameName(addUser) {
+        return user.name === addUser.name;
+    }
+        
+    if (users.find(isSameName)) {
+       throw new Error('This user is already registered');
+    }
+    
+    users.push(user);
+    
+    res.send(user);
+});
+
+
+router.put('/users/:name', (req, res, next) => {
+    
+    const user = users.find( (user) => {
+        return user.name === req.params.name; // don`t work without return 
+        //user.name === req.params.name;
+    });
+
+    let schema = Joi.object({
+        scoreTime: Joi.string().min(1).max(6).required(),
+        scoreEat: Joi.string().min(1).max(6).required()
+    });
+
+    let resultValidation =  Joi.validate(req.body, schema);
+    
+    if (resultValidation.error) {
+        throw new Error(resultValidation.error);
+    }
+    else {
+        
+        user.scoreTime = req.body.scoreTime;
+        user.scoreEat = req.body.scoreEat;
+    
+        res.send(user);
+    }
+});
+
+
+router.delete('/users/:name', (req, res, next) => {
+    
+    users = users.filter( (user) => {
+        return user.name !== req.params.name;
+    });
+
+    res.send(users);
+});
+
+
+module.exports = router;
