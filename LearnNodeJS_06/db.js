4d44f0a51e77c7819384402ddc4e7392486e6283
Oleh <oleh.fylypiv@gmail.com>
2017-07-26 22:48:01 +0300

    "LearnNodeJS: Improved API, added an authentication using Passport.js"

---
+++ LearnNodeJS_06/db.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+        id: '1500397202725',
+        name: 'Oleh',
+        password: '1234567890',
+        email: 'oleh.fylypiv@gmail.com',
+        role: 'user'
+    }
+];
+
+
